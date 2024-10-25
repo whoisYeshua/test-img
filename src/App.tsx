1147e676f74590b9aef57d7842ef91093b441b1f
whoisYeshua <alien19987@gmail.com>
2024-10-25 10:23:53 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src="https://www.blackbookmotorsport.com/wp-content/uploads/2024/08/nascar-charter-agreement-sponsorship-2025.jpg" />
+      </div>
+    </>
+  )
+}
+
+export default App
